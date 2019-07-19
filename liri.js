9558b618386a3401f9a7afaae3b1f4755b8131ff
Nathan Hill <pjsstp@gmail.com>
2019-07-18 23:11:28 -0400

    initial commit with basic packages and skeleton functionality

---
+++ liri.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+const goodCommands = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'];
+
+let theCommand = process.argv[2]
+let theArgument = process.argv[3]
+
+const getMeSpotify = songName => {
+    console.log(`spotify thingy ${songName}`)
+}
+
+const getMeConcert = artistName => {
+    console.log(`concert thingy ${artistName}`)
+}
+
+const getMeMovie = movieName => {
+    console.log(`movie thingy ${movieName}`)
+}
+
+const doRandom = () => {
+    console.log('random.txt thingy');
+}
+
+if (goodCommands.includes(theCommand)) {
+    switch (theCommand) {
+        case 'concert-this': 
+            getMeConcert(theArgument)
+            break
+        case 'spotify-this-song':
+            getMeSpotify(theArgument)
+            break
+        case 'movie-this':
+            getMeMovie(theArgument)
+            break
+        case 'do-what-it-says':
+            doRandom()
+            break
+        default :
+            console.log('Something went wrong...')
+            break
+    }
+}
