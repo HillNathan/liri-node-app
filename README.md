4b89201888b9451e64a96f01a57997a99e79da4d
Nathan Hill <pjsstp@gmail.com>
2019-07-25 11:48:57 -0400

    finished ReadME editing with images, mae minor updates to liri.js to include more robust error handling and project flow

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # liri-node-app
+Welcome to my LIRI App, build in Node.js. This is a Language Interpretation Recognition Interface, built in a command-line environment. This is a text-only app, which hits one of three APIs based on input from the user: Spotify API, Bandsintown API, and the OMDB Api. To run this on your own, you will need to set up your own .env file like this:
+
+    SPOTIFY_ID=*your-spotify-api-key-here*
+    SPOTIFY_SECRET=*your-spotify-secret-key-here*
+
+Once you set this file up you should be able to have access to all of the APIs used in this app. Now that we have set the stage, let's take a look at how to use my LIRI!
+
+The initial version of liri took the commands as additional arguments on the command line, which worked but was not very user-friendly. One mis-spelled command and the app didn't work. Using the Inquirer package allows us to take the correct commands using a list, which saves a lot of that kind of headache. Here is a screenshot of our starting list: 
+
+<img src="images/LIRI-initial.jpg">
+
+As you can see from this screenshot, we have four starting options: "Concerts", "Spotify", "Movies", and "random.txt". You can toggle between these choices by using the up and down arrows, and register your choice by pressing the <ENTER> key. I will come back to the random.txt in a bit, so for now let's start with the first option, "Concerts". When you press the <ENTER> key, you are asked for additional input. Here, you may enter a band or performer to search for. If no input is received, the API will automatically search for the Rolling Stones:
+
+<img src="images/LIRI-concert-input.jpg">
+
+You will be asked to confirm your input by typing (Y/n). If you simply press <ENTER> at this input, the default is Yes. If you decide that your input is not what you wanted, pressing (n) will exit you from the program and you will be able to run it again to enter what you want. Once you confirm your input and press Y or <ENTER>, you will receive a list of the upcoming shows for that band from the BandsinTown API. 
+
+<img src="images/LIRI-concert-response.jpg">
+
+The Spotify API functions similarly to the Concerts API. It will return a list of songs that match the search term, as shown below:
+
+<img src="images/LIRI-spotify-initial.jpg"><br>
+<img src="images/LIRI-spotify-input.jpg"><br>
+<img src="images/LIRI-spotify-response.jpg"><br>
+
+The movie search functions slightly differently, in that it will only return a single movie for each search. In the image below, you can see the full data input and response for the LIRI Movies search:
+
+<img src="images/LIRI-movies-full.jpg">
+
+<h2>Random.txt</h2>
+
+Random.txt is set up to not really be random, but to take input from the "random.txt" file. It takes two inputs, separated by commas, using the following criteria:
+
+Input one must be the module you want to search for. Either "spotify", "concerts", or "movies". The second one must be your search term. The search term <b>cannot contain commas</b>, or it will  not work properly. The search term does not have to be wrapped in quotes, since the LIRI separates based on the commas, not spaces. The initial set of terms is:
+
+    spotify,I Want it That Way
+
+So play with the random.txt file, play around with the LIRI and have fun!
+
+Nathan Hill
--- liri.js
+++ liri.js
@@ -4,14 +4,56 @@ const moment = require("moment")
 const axios = require("axios")
 const fs = require("fs")
 const Spotify = require("node-spotify-api")
+const inquirer = require("inquirer")
 const spotifyApi = new Spotify({
     id: keys.spotify.id,
     secret: keys.spotify.secret
 })
 
-const goodCommands = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'];
-let theCommand = process.argv[2]
-let theArgument = process.argv[3]
+inquirer.prompt([
+    {   type: "list",
+        message: "What module do you want to use?",
+        choices: ["Concerts", "Spotify", "Movies", "random.txt"],
+        name: "module"
+    },
+    ]).then(inqResponse => {
+        if (inqResponse.module === 'random.txt' ) {
+            doRandom()
+        }
+        else {
+            inquirer.prompt([
+                {   type: "input",
+                message: "What do you want to search for? ",
+                name: "searchTerm"
+                }
+            ]).then(searchResponse => {
+                inquirer.prompt([
+                    {   type: "confirm",
+                        message: `So you want to search ${inqResponse.module} for "${searchResponse.searchTerm}"?`,
+                        name: "confirm",
+                        default: true }
+                    ]).then(finalResponse => {     
+                        if (finalResponse.confirm) {
+                            switch (inqResponse.module) {
+                                case 'Concerts': 
+                                    getMeConcert(searchResponse.searchTerm)
+                                    break
+                                case 'Spotify':
+                                    getMeSpotify(searchResponse.searchTerm)
+                                    break
+                                case 'Movies':
+                                    getMeMovie(searchResponse.searchTerm)
+                                    break
+                                default :
+                                    console.log('Ooops. Something went wrong.')
+                                    break
+                            }
+                        }
+                    })
+                })
+            }
+        })
+
 
 const getMeSpotify = songName => {
     if (!songName) songName = "The Sign"
@@ -20,17 +62,23 @@ const getMeSpotify = songName => {
     spotifySearch.query = songName
     spotifyApi.search(spotifySearch, (err, data) => {
         if (err) return console.log('Error occurred: ' + err)
-        data.tracks.items.forEach(element => {
-            console.log(`Artist: ${element.artists[0].name}`)
-            console.log(`Song Name ${element.name}`)
-            console.log(`Album Name ${element.album.name}`)
-            console.log(`Preview URL: ${element.external_urls.spotify}`)
-            console.log('===============================================')
-        })
+        if (data.tracks.items.length === 0) {
+            console.log('No tracks were found for that search term.')
+        }
+        else {
+            data.tracks.items.forEach(element => {
+                console.log(`Artist: ${element.artists[0].name}`)
+                console.log(`Song Name ${element.name}`)
+                console.log(`Album Name ${element.album.name}`)
+                console.log(`Preview URL: ${element.external_urls.spotify}`)
+                console.log('===============================================')
+            })
+        }
     })
 }
 
 const getMeConcert = artistName => {
+    if (!artistName) artistName = "Rolling Stones"
     let theURL = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=codingbootcamp&date=upcoming`
     axios.get(theURL)
         .then(results => {
@@ -40,6 +88,9 @@ const getMeConcert = artistName => {
                 console.log(`Concert Date: ${moment(element.datetime).format('MM/DD/YYYY')}`)
                 console.log('===============================================')
             })
+        }).catch(err => {
+            console.log('catch function')
+            console.log(err.response.data.errorMessage)
         })
 }
 
@@ -47,14 +98,19 @@ const getMeMovie = movieName => {
     if (!movieName) movieName = 'Mr Nobody'
     axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${movieName}`)
         .then(results => {
-            console.log(`Title: ${results.data.Title}`)
-            console.log(`Year: ${results.data.Year}`)
-            console.log(`IMDB Rating: ${results.data.Ratings[0].Value}`)  // IMDB
-            console.log(`Rotten Tomatoes Rating: ${results.data.Ratings[2].Value}`)  // Rotten Tomatoes
-            console.log(`Country: ${results.data.Country}`)
-            console.log(`Language: ${results.data.Language}`)
-            console.log(`Plot: ${results.data.Plot}`)
-            console.log(`Actors ${results.data.Actors}`)
+            if (results.data.Response !== "False") {
+                console.log(`Title: ${results.data.Title}`)
+                console.log(`Year: ${results.data.Year}`)
+                console.log(`IMDB Rating: ${results.data.Ratings[0].Value}`)  // IMDB
+                console.log(`Rotten Tomatoes Rating: ${results.data.Ratings[2].Value}`)  // Rotten Tomatoes
+                console.log(`Country: ${results.data.Country}`)
+                console.log(`Language: ${results.data.Language}`)
+                console.log(`Plot: ${results.data.Plot}`)
+                console.log(`Actors ${results.data.Actors}`)
+            }
+            else {
+                console.log(results.data.Error)
+            }
         })
 }
 
@@ -62,37 +118,17 @@ const doRandom = () => {
     fs.readFile("random.txt", "utf8", (error, data) => {
         if (error) return console.log(error.code);
         let stuff = data.split(",")
-        switch (stuff[0]) {
-            case 'concert-this': 
+        switch (stuff[0].toLowerCase()) {
+            case 'concerts': 
                 getMeConcert(stuff[1])
                 break
-            case 'spotify-this-song':
+            case 'spotify':
                 getMeSpotify(stuff[1])
                 break
-            case 'movie-this':
+            case 'movies':
                 getMeMovie(stuff[1])
                 break
             default:
                 console.log("Ooops. Something went wrong.") }
-      });
-}
-
-if (goodCommands.includes(theCommand)) {
-    switch (theCommand) {
-        case 'concert-this': 
-            getMeConcert(theArgument)
-            break
-        case 'spotify-this-song':
-            getMeSpotify(theArgument)
-            break
-        case 'movie-this':
-            getMeMovie(theArgument)
-            break
-        case 'do-what-it-says':
-            doRandom()
-            break
-        default :
-            console.log('Ooops. Something went wrong.')
-            break
-    }
+      })
 }
